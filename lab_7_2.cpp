85764281fe6d680aa0bccc6c31bc78e96203b336
Unknown <sarisa.050241@gmail.com>
2017-12-22 13:52:58 +0700

    ข้อ 2

--- lab_7_2.cpp
+++ lab_7_2.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main () {
 
-  cout << "Sum of Natural Numbers using loop" << endl << endl ;
+  cout << "Sum of Natural Numbers using loop" << endl << endl;
 
   int i = 0;
   int posInputNumber = 0;
   int sum = 0;
+  cin >> posInputNumber;
 
   // TODO: เพิ่มการรับข้อมูลจากผู้ใช้ ให้ posInputNumber
 
   // FIXME: แก้ไขการทำงานของ loop for ให้แสดงผลรวมตั้งแต่ 0 - ค่าที่ผู้ใช้รับมา (posInputNumber)
-  for (i = 0; i <= 10; i++) {
+  for (i = 0; i <= posInputNumber ; i++) {
     sum += i;
   }
 
